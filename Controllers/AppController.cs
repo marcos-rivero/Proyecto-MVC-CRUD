6f8bcc36b3c2f468f07870ac0c289f3c8a58418e
marcos rivero <marcosrivero_d4@hotmail.com>
2023-10-31 19:34:48 -0300

    proyecto original

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,16 +1,28 @@
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
-
-public class AppController : Controller{
-    public IActionResult Index(){          
-		return View(); 
-	}
-    public IActionResult Edit(){
-        throw new InvalidOperationException();       
-        return View();
-    }
-    [HttpGet("about")]
-    public IActionResult About(){
-        return View();
+using ProyectoMVC.ViewModel;
+namespace ProyectoMVC.Controllers
+{
+    public class AppController : Controller{
+        private static List<PeliculaViewModel> _peliculas = new List<PeliculaViewModel>();
+        public IActionResult Index(){          
+            return View(_peliculas); 
+        }
+        public IActionResult Edit(){      
+            return View();
+        }
+        [HttpGet("about")]
+        public IActionResult About(){
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(PeliculaViewModel modelo){
+            if(ModelState.IsValid){
+                _peliculas.Add(modelo);
+                return RedirectToAction(nameof(Index));
+                // codigo para insertar en BD
+            }
+            return View();
+        }
     }
 }
